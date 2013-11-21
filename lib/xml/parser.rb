d4b32be8751c58154b48761ce91bbf33e725cc17
yortz <skylarking420@gmail.com>
2013-11-21 11:23:00 +0100

    Parsing biomart xml config file to retrieve a list of marts.

--- lib/xml/parser.rb
+++ lib/xml/parser.rb
@@ -1,12 +1,23 @@
 FILENAME = "GenePrioritizationPrototype"
 PATH = "/Users/federicocortini/code/HSR/biomart-rc7/#{FILENAME}.xml"
 
+require 'nokogiri'
+
 module XML
   class Parser
     attr_accessor :path
 
     def initialize(path=PATH)
       @path = path
+      @doc = Nokogiri::XML(File.open(@path))
+    end
+
+    def get_element(element)
+      @menu = []
+      @doc.xpath(element).each do |e|
+        @menu << e.attr('displayname')
+      end
+      @menu
     end
 
   end
--- spec/xml_parser_spec.rb
+++ spec/xml_parser_spec.rb
@@ -1,10 +1,20 @@
 require "spec_helper"
 
 describe "XML Parser" do
-  XML_PATH = "fixtures/dummy.xml"
+  XML_PATH = "spec/fixtures/dummy.xml"
   let(:parser) { XML::Parser.new(XML_PATH) }
 
   it "has a default path" do
     expect(parser.path).to eq(XML_PATH)
+  end
+
+  it "builds the menu" do
+    menu = parser.get_element("//mart//config[@master='false']")
+    expect(menu).to eq(["Gene Vega STOCA",
+                       "Geni Alata",
+                       "GENE VEGA STICA",
+                       "Gennaro",
+                       "GENE THAT BOTHERS",
+                       "GEnI0"])
   end
 end
